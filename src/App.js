9e01f858af3cb95a7bafbadda7a9e7efeb248040
Especially <_j@live.ca>
2020-09-03 09:27:56 -0400

    Initialized repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Section from './components/Section/Section';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="hero">
+        <div className="hero__content">
+          <h1 className="hero__title">Josh Walters</h1>
+          <div className="hero__avatar"></div>
+          <p className="hero__desc">Full-Stack Web Developer, meme lover and I often like to think that I'm a photographer when I'm taking pics with my phone.</p>
+          <div className="hero__socials">
+            <a className="hero__link" target="_blank" rel="noopener noreferrer" href="http://linkedin.com/in/joshwalters94">
+              <i className="fab fa-linkedin hero__icon" title="Check out my LinkedIn!"></i>
+            </a>
+            <a className="hero__link" target="_blank" rel="noopener noreferrer" href="https://github.com/especially">
+              <i className="fab fa-github-square hero__icon" title="Check out my GitHub!"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <main className="container">
+      <section className="container__section">
+          <h1>About</h1>
+          <div className="">
+
+          </div>
+        </section>
+        <section className="section accordion">
+          <h1>Projects</h1>
+          <div className="accordion__block">
+            <h2>Project Name</h2>
+          </div>
+          <div className="accordion__block accordion__block--collapsed">
+            <h2>Project Name</h2>
+          </div>
+        </section>
+      </main>
+      <article>
+        <section className="section">
+          <h1>About</h1>
+        </section>
+        <section className="section accordion">
+          <h1>Projects</h1>
+          <div className="accordion__block">
+            <h2>Project Name</h2>
+          </div>
+          <div className="accordion__block accordion__block--collapsed">
+            <h2>Project Name</h2>
+          </div>
+        </section>
+      </article>
+      <article>
+        <section className="section">
+          <h1>About</h1>
+        </section>
+        <section className="section accordion">
+          <h1>Projects</h1>
+          <div className="accordion__block">
+            <h2>Project Name</h2>
+          </div>
+          <div className="accordion__block accordion__block--collapsed">
+            <h2>Project Name</h2>
+          </div>
+        </section>
+      </article>
+      <article>
+        <section className="section">
+          <h1>About</h1>
+        </section>
+        <section className="section accordion">
+          <h1>Projects</h1>
+          <div className="accordion__block">
+            <h2>Project Name</h2>
+          </div>
+          <div className="accordion__block accordion__block--collapsed">
+            <h2>Project Name</h2>
+          </div>
+        </section>
+      </article>
+      <article>
+        <section className="section">
+          <h1>About</h1>
+        </section>
+        <section className="section accordion">
+          <h1>Projects</h1>
+          <div className="accordion__block">
+            <h2>Project Name</h2>
+          </div>
+          <div className="accordion__block accordion__block--collapsed">
+            <h2>Project Name</h2>
+          </div>
+        </section>
+      </article>
+    </>
   );
 }
 
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section = ({title}) => {
+
+
+    return (
+        <section className="container__section">
+          <h1>{title}</h1>
+        </section>
+    );
+}
+
+export default Section;
