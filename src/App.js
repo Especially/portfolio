1387c940cab27bc0419b3fd8c88599da6b66a9d3
Josh Walters <_J@Mac-Studio.local>
2023-11-29 20:43:32 -0500

    chore: Bump react router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import Main from './pages/Main/Main';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProjectDetails from './components/ProjectDetails/ProjectDetails';
-import Toggle from "./components/Toggler/Toggler"
-import  {useDarkMode} from "./components/Helpers/";
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './styles/globalStyles';
-import { lightTheme, darkTheme } from './styles/Theme';
+import React from "react";
+import Main from "./layouts/Main/Main";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProjectDetails from "./components/ProjectDetails/ProjectDetails";
+import Toggle from "./components/Toggler/Toggler";
+import { useDarkMode } from "./hooks/useMeasure";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/globalStyles";
+import { lightTheme, darkTheme } from "./styles/Theme";
+import ErrorPage from "./pages/404";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "/project/:id", element: <ProjectDetails /> }],
+  },
+]);
 
 function App() {
   const [theme, themeToggler] = useDarkMode();
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
 
   return (
     <ThemeProvider theme={themeMode}>
       <>
         <GlobalStyles />
         <Toggle theme={theme} toggleTheme={themeToggler} />
-        <Router>
-          <Switch>
-            <Route path='/' render={() => {
-              return (
-                <>
-                  <Main />
-                  <Route path='/project/:id' component={ProjectDetails} />
-                </>
-              )
-            }} />
-          </Switch>
-        </Router>
+        <RouterProvider router={router} />
       </>
     </ThemeProvider>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
