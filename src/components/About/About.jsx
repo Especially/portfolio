46d02313e6a50c0812005170942cd118f8b797f2
Josh Walters <me@joshwalters.ca>
2023-12-01 00:16:34 -0500

    chore: Test add much

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
-import {Section, Paragraph} from '../../styles/globalStyles';
-import {Title, Highlight} from './s-about';
+import React from "react";
+import { Section, Paragraph } from "../../styles/globalStyles";
+import { Title, Highlight } from "./s-about";
 
 const About = () => {
-
-
   return (
     <Section>
-      <Title>Who is <Highlight>Josh</Highlight>?</Title>
+      <Title>
+        Who is <Highlight>Josh</Highlight>?
+      </Title>
       <div className="about__holder">
         <div className="about__content">
-          <Paragraph>Ever since I was about 10 years old, I've dabbled in developing websites, whether it be for leisure or to create concepts which could make one's life much easier.</Paragraph>
-          <Paragraph>I think I started to actually realize my talent when it came to converting my father's way of handling his business into a much more efficient and digital way! I couldn't fathom how he managed to do everything on pen and paper, and so begun my journey (and love) of developing web applications that can make people's lives much, much easier.</Paragraph>
-          <Paragraph>Aside from developing, I'm very much an extrovert, so in my off time, I'm usually off on an adventure with some friends, or taking one of my nature walks and pretending that I'm a photographer (or videographer, depending on my mood) with my iPhone.</Paragraph>
+          <Paragraph>
+            Ever since I was about 10 years old, I've dabbled in developing
+            websites, whether it be for leisure or to create concepts which
+            could make one's life much, much easier.
+          </Paragraph>
+          <Paragraph>
+            I think I started to actually realize my talent when it came to
+            converting my father's way of handling his business into a much more
+            efficient and digital way! I couldn't fathom how he managed to do
+            everything on pen and paper, and so begun my journey (and love) of
+            developing web applications that can make people's lives much, much
+            easier.
+          </Paragraph>
+          <Paragraph>
+            Aside from developing, I'm very much an extrovert, so in my off
+            time, I'm usually off on an adventure with some friends, or taking
+            one of my nature walks and pretending that I'm a photographer (or
+            videographer, depending on my mood) with my iPhone.
+          </Paragraph>
         </div>
       </div>
     </Section>
   );
-}
+};
 
 export default About;
